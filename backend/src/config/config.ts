69c7e8de38becf7b69be119c80a06e33b2a78cc2
vishnu <vishnuprasadvv24@gamil.comgit config --global user.name vishnuprasadvv>
2025-02-08 10:27:06 +0530

    added options method

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface IConfig {
-    App : {
-        PORT : number
-    }
-}
+
 export const config = {
     App : {
         PORT : process.env.PORT || 5000,
@@ -20,7 +16,7 @@ export const config = {
     cors: {
         CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
         ALLOWED_HEADERS : ['Content-Type', 'Authorization'],
-        ALLOWED_METHODS : ["GET", "POST", "DELETE", "PUT","PATCH"],
+        ALLOWED_METHODS : ["GET", "POST", "DELETE", "PUT","PATCH", "OPTIONS"],
         CREDENTIALS : true
     }
 }
